56bf89b3a2b70e84d3db1b701aced3ce533cffb6
Yummy-sk <60822846+Yummy-sk@users.noreply.github.com>
2021-05-13 18:47:28 +0900

    상단 메뉴와 사이드 메뉴 구성

--- src/Component/NavigationBar/NavBar.js
+++ src/Component/NavigationBar/NavBar.js
@@ -1,15 +1,26 @@
 import React, {useState} from 'react';
 import './NavBar.css';
 import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
 
 export default function NavBar() {
+    const [open, setOpen] = useState(false);
+    const onClick = () => setOpen(!open);
 
     return (
         <div className='NavBar'>
-            <FaIcons.FaBars/>
-            <ul>
-                <li>안녕</li>
-            </ul>
+
+            <div className='menu-bars'>
+                <FaIcons.FaBars onClick={onClick}/>
+            </div>
+
+            <nav className={open ? 'nav-menu active': 'nav-menu'}>
+                <ul className='nav-menu-items'>
+                    <li className='navbar-toggle'>
+                        <AiIcons.AiOutlineClose className='close-btn' onClick={onClick} />
+                    </li>
+                </ul>
+            </nav>
         </div>
     );
 }
