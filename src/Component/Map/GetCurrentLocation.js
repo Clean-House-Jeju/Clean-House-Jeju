2115fb01bfeae804784fd9a2dec4c8d72fd380c8
IGhos-P <dndb3599@gmail.com>
2021-05-19 16:24:34 +0900

    커스텀 저장

--- src/Component/Map/GetCurrentLocation.js
+++ src/Component/Map/GetCurrentLocation.js
@@ -6,7 +6,7 @@ export default function GetCurrentLocation(map) {
     if (navigator.geolocation) {
 
         // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-        navigator.geolocation.getCurrentPosition(function(position) {
+        navigator.geolocation.getCurrentPosition(function (position) {
 
             var lat = position.coords.latitude, // 위도
                 lon = position.coords.longitude; // 경도
@@ -27,7 +27,7 @@ export default function GetCurrentLocation(map) {
         displayMarker(locPosition, message);
     }
 
-// 지도에 마커와 인포윈도우를 표시하는 함수입니다
+    // 지도에 마커와 인포윈도우를 표시하는 함수입니다
     function displayMarker(locPosition, message) {
 
         // 마커를 생성합니다
@@ -41,8 +41,8 @@ export default function GetCurrentLocation(map) {
 
         // 인포윈도우를 생성합니다
         var infowindow = new kakao.maps.InfoWindow({
-            content : iwContent,
-            removable : iwRemoveable
+            content: iwContent,
+            removable: iwRemoveable
         });
 
         // 인포윈도우를 마커위에 표시합니다
--- src/Component/Map/loadMultiMarker.js
+++ src/Component/Map/loadMultiMarker.js
@@ -1,5 +1,25 @@
+import './CustomOverlay.css'
 
 const { kakao } = window;
+
+var content = '<div class="wrap">' +
+    '    <div class="info">' +
+    '        <div class="title">' +
+    '            카카오 스페이스닷원' +
+    '            <div class="close" onclick="closeOverlay()"></div>' +
+    '        </div>' +
+    '        <div class="body">' +
+    '            <div class="img">' +
+    '                <img src="https://cfile181.uf.daum.net/image/250649365602043421936D" width="73" height="70">' +
+    '           </div>' +
+    '            <div class="desc">' +
+    '                <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>' +
+    '                <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>' +
+    '                <div><a href="https://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>' +
+    '            </div>' +
+    '        </div>' +
+    '    </div>' +
+    '</div>';
 
 export default function LoadMultiMarker(map, data) {
     console.log(data);
@@ -23,32 +43,46 @@ export default function LoadMultiMarker(map, data) {
 
         });
 
+
+
+        var overlay = new kakao.maps.CustomOverlay({
+            content: content,
+            map: map,
+
+        });
+
         var infowindow = new kakao.maps.InfoWindow({
-            content: data[i].location, // 인포윈도우에 표시할 내용
+            //content: data[i].location, // 인포윈도우에 표시할 내용
+            content: content
         });
 
         kakao.maps.event.addListener(
             marker,
             "mouseover",
             makeOverListener(map, marker, infowindow)
+            //makeOverListener(map, marker, overlay)
         );
         kakao.maps.event.addListener(
             marker,
             "mouseout",
             makeOutListener(infowindow)
+            // makeOutListener(overlay)
         );
     };
+
     // 인포윈도우를 표시하는 클로저를 만드는 함수입니다
-    function makeOverListener(map, marker, infowindow) {
+    function makeOverListener(map, marker, customOverlay) {
         return function () {
             infowindow.open(map, marker);
+            //customOverlay.setMap(map);
         };
     }
 
     // 인포윈도우를 닫는 클로저를 만드는 함수입니다
-    function makeOutListener(infowindow) {
+    function makeOutListener(customOverlay) {
         return function () {
             infowindow.close();
+            //customOverlay.setMap(null);
         };
     }
 
