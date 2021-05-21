b12cce083badeec36afa48ddcb19de53caf869b9
Yummy-sk <60822846+Yummy-sk@users.noreply.github.com>
2021-05-22 05:03:35 +0900

    네비게이션 바 css 오류..

--- src/Component/NavigationBar/NavBar.js
+++ src/Component/NavigationBar/NavBar.js
@@ -2,18 +2,29 @@ import React, {useState} from 'react';
 import './NavBar.css';
 import * as FaIcons from 'react-icons/fa';
 import SideBar from "./SideBar";
+import LocationCardList from "./LocationCardsList";
+import {useSelector} from "react-redux";
+import filterKeyword from "../Map/filterKeyword";
 
 const NavBar = React.memo(() => {
+    const {data} = useSelector(state => state.getDatas.datas);
+    const {text} = useSelector(state => state.keyword);
     const [open, setOpen] = useState(false);
     const onClick = () => setOpen(!open);
-
     return (
-        <div className='NavBar'>
-
-            <div className='menu-bars'>
+        <div className='menu-container'>
+            <div className='search-box'>
                 <FaIcons.FaBars onClick={onClick}/>
+                <form>
+                    <input
+                        type="text"
+                        className='input-texts'
+                    />
+                </form>
             </div>
-
+            <div className='content-box'>
+                <LocationCardList data={filterKeyword(data, text)}/>
+            </div>
             <SideBar open={open} onClick={onClick}/>
         </div>
     );
