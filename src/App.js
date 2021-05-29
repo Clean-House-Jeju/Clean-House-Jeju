02d1c6c4bc0d0ecb442865d5f2edee3b80dd2f87
Yummy-sk <60822846+Yummy-sk@users.noreply.github.com>
2021-05-29 20:33:55 +0900

    로딩시 화면에 랜더링

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Map from "./Component/Map/Map";
 import {Reset} from "styled-reset";
 import {useDispatch, useSelector} from "react-redux";
 import {getInfo} from "./Modules/getDatas";
+import Loading from "./Component/Loading/Loading";
 
 export default function App() {
     const {data, loading, error} = useSelector(state => state.getDatas.datas);
@@ -19,7 +20,7 @@ export default function App() {
         fn();
     }, [dispatch]);
 
-    if (loading) return <div>로딩중..</div>
+    if (loading) return <Loading/>
     if (error) return <div>에러발생</div>
     if (!data) return <div>데이터가 없어요</div>;
 
