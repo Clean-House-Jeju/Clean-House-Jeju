459cfa9d6721a3a6cfc9178b9a0c9ffcd69daf72
Yummy-sk <60822846+Yummy-sk@users.noreply.github.com>
2021-06-01 21:27:43 +0900

    Tab바 구성

---
+++ src/Component/NavigationBar/SideBar/WeeklyInform/DayInfoContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {recycleData} from "../../../../lib/showDataByDate";
+import DayInfoCard from "./DayInfoCard";
+import './DayInfoContainer.css';
+export default function DayInfoContainer() {
+    return (
+        <div className='day-info-container'>
+            {
+                recycleData.map(data => <DayInfoCard day={data.day} type={data.type} color={data.color}/>)
+            }
+        </div>
+    );
+}
--- src/Component/NavigationBar/SideBar/WeeklyInform/WeeklyInfom.js
+++ src/Component/NavigationBar/SideBar/WeeklyInform/WeeklyInfom.js
@@ -1,18 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styled from "styled-components";
 import './WeeklyInform.css';
-import DayInfoCard from "./DayInfoCard";
-import {recycleData} from "../../../../lib/showDataByDate";
+import DayInfoContainer from "./DayInfoContainer";
+
+const Container = styled.div`
+    margin-top: 30px;
+    width: 100%;
+`
+const Nav = styled.div`
+  display: flex;
+  position: relative;
+  background-color: red;
+`
+const Tab = styled.div`
+  width: 50%;
+  height: 60px;
+  line-height: 60px;
+  text-align: center;
+  cursor: pointer;
+  transition: color 0.15s ease-in;
+  z-index: 1000;
+`
+const Glider = styled.span`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 60px;
+  width: 50%;
+  background-color: #fff;
+  transition: 0.25s ease-out;
+  transform: translate3D(${props => props.index * 100}%, 0, 0);
+`;
 
 export default function WeeklyInform() {
+    const [index, setIndex] = useState(0);
+    const toggleState = (index) => {
+        setIndex(index);
+    }
+    const menu = ['요일별', '항목별'];
+
     return (
-        <div className='weekly-inform-container'>
+        <Container>
+            <Nav>
+                {menu.map((text, i) => (
+                    <Tab ket={i} onClick={() => toggleState(i)}>
+                        {text}
+                    </Tab>
+                ))}
+                <Glider index={index} />
+            </Nav>
             {
-                recycleData.map(data => <DayInfoCard day={data.day} type={data.type} color={data.color}/>)
+                index === 0? <DayInfoContainer/>: <div>안녕</div>
             }
-
-            <ul className=''>
-
-            </ul>
-        </div>
+        </Container>
     );
 }
