af19e8d602fa33cfc5c0a0d1e0161ba6cae55036
IGhos-P <dndb3599@gmail.com>
2021-06-04 13:46:56 +0900

    리엑트 컴포넌트 전

--- src/Component/InformationCard/CleanOverlay.js
+++ src/Component/InformationCard/CleanOverlay.js
@@ -23,18 +23,20 @@ function CleanOverlay(data, i) {
     var clean = '<div class="wrap">' +
         '    <div class="info">' +
         '        <div class="title">' +
-        '            클린 하우스' +
-        '<div class="close" onclick="closeOverlay()" title="닫기"></div>' +
+        '            클린하우스' +
+        '            <div class="close" onclick="closeOverlay()" title="닫기"></div>' +
         '        </div>' +
         '        <div class="body">' +
+        '           </div>' +
         '            <div class="desc">' +
-        '                <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>' +
-        '                <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>' +
-        '                <div><a href="https://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>' +
+        '                <div class="location">제주특별자치도 제주시 첨단로 242</div>' +
+        '                <div class="runtime">(우) 63309 (지번) 영평동 2181</div>' +
+        '                <div><a href=https://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>' +
         '            </div>' +
         '        </div>' +
         '    </div>' +
         '</div>';
+
 
     return clean;
 }
--- src/Component/Map/loadMultiMarker.js
+++ src/Component/Map/loadMultiMarker.js
@@ -111,7 +111,7 @@ export default function LoadMultiMarker(map, data) {
     var clusterer = new kakao.maps.MarkerClusterer({
         map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체 
         averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정 
-        minLevel: 6 // 클러스터 할 최소 지도 레벨 
+        minLevel: 4 // 클러스터 할 최소 지도 레벨 
     });
 
     // 클러스터러에 마커들을 추가합니다(마커 클러스터러 관련)
