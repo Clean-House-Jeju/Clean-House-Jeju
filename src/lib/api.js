fe891e80425409510d149339e4b71dffee3bc428
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-03-09 17:26:16 +0900

    chore: api 루트 변경

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_ROOT = "https://raw.githubusercontent.com/Woongstar/data_repo/main/cleanhouseproject/time/cleandatasplit.json";
+const API_ROOT = "https://gist.githubusercontent.com/Yummy-sk/162dd1e4349ebf821f43db6c3c67f744/raw/ed25686c4f36e2b1474a8eeab2fa52837bdb5d93/jeju_clean_house";
 
 export const getData = () => {
     return axios.get(API_ROOT);
